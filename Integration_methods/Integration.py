e8d028787001d62535f3bb016970fdccbe24e050
GertCell <gertseli51@gmail.com>
2022-09-26 22:13:18 +0700

    I've added a graphs, showing the differens between obtained and real values

--- Integration_methods/Integration.py
+++ Integration_methods/Integration.py
@@ -1,7 +1,9 @@
 # Trapezoid
 
 from scipy import *
-
+from scipy import integrate
+import numpy as np
+import matplotlib.pyplot as plt
 # Function
 
 
@@ -11,7 +13,7 @@ def f(x):
 
 # Real solution
 
-v = integrate.quad(f, -1, 1)
+v = np.pi/2
 print('real value', v)
 
 # Parameters
@@ -23,6 +25,8 @@ b = -1
 
 n = 100
 d = (b-a)/n
+S1 = []
+S2 = []
 
 
 def trapz():
@@ -34,7 +38,9 @@ def trapz():
         xi_1 = xi + d
         s = s1
         s1 = s + (f(xi_1) + f(xi)) * d
+        S1.append(v + s1/2)
     print('Trapz result', -s1/2)
+    return S1
 
 
 def simpson():
@@ -48,8 +54,18 @@ def simpson():
         xi_2 = xi_1 + d
         s = s1
         s1 = s + d / 3 * (f(xi) + 4 * f(xi_1) + f(xi_2))
+        S2.append(v + s1/2)
     print('Simpson result', -s1/2)
+    return S2
 
 
 trapz()
 simpson()
+
+y = np.arange(0, 99)
+
+plt.plot(y, S1, 'r--', y, S2, 'r--')
+# ax[1].plot(S2)
+
+plt.show()
+
---
+++ Precision/anim for Int.py
@@ -0,0 +1,56 @@
+from numpy import *
+from matplotlib.pyplot import *
+# This code works only in jupyter notebook
+%matplotlib notebook
+
+def f(x):
+    return 1 / (x ** 2 + 1)
+
+
+S1 = []
+S2 = []
+I = []
+v = np.pi / 2
+
+# Parameters
+
+a = 1
+b = -1
+
+# Segment splitting
+
+n = 100
+d = (b - a) / n
+
+subplot(111)
+ln, = plot(I, S2)
+ln1, = plot(I, S1)
+axis([0, 100, -1, 1])
+
+xi = a
+xi_1 = xi + d
+xi_2 = xi_1 + d
+s1 = d / 3 * (f(xi) + 4 * f(xi_1) + f(xi_2))
+for i in range(0, n - 1):
+    xi = xi_1
+    xi_1 = xi + d
+    xi_2 = xi_1 + d
+    s = s1
+    s1 = s + d / 3 * (f(xi) + 4 * f(xi_1) + f(xi_2))
+    S2.append(v + s1 / 2)
+    I.append(i)
+
+xi = a
+xi_1 = xi + d
+s1 = (f(xi_1) + f(xi)) * d
+for i in range(0, n - 1):
+    xi = xi_1
+    xi_1 = xi + d
+    s = s1
+    s1 = s + (f(xi_1) + f(xi)) * d
+    S1.append(v + s1 / 2)
+
+ln.set_data(I, S2)
+ln1.set_data(I, S1)
+gcf().canvas.draw()
+
