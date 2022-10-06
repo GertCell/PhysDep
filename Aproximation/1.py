9bd45ff407328f87b700702074d1c2cada6ba85c
GertCell <gertseli51@gmail.com>
2022-10-06 22:34:37 +0700

    работает бля

---
+++ Aproximation/1.py
@@ -0,0 +1,34 @@
+from matplotlib.pyplot import *
+import numpy as np
+
+N = 100
+
+H = 10
+T = 10
+
+a = 0.5
+h = a * H / N
+t = T / N
+
+X = np.arange(0, H, h)
+U = [3] * len(X)
+U[0] = 5
+UU = []
+
+fig, ax = subplots(1)
+axis([0, 10, 0, 5])
+ax.set_title('Wave')
+ax.set_xlabel('Distance')
+ax.set_ylabel('Wave Speed')
+
+for time in np.arange(0, T, t):
+
+    UU = U
+    for i in range(0, N + 1):
+        U[i] = UU[i] + a*(UU[i - 1] - UU[i])
+    ax.plot(X, U)
+    fig.canvas.show()
+    cla()
+
+
+
