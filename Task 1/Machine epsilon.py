4ae9c7a06c617e8df5365a80a78f1b0ae39b5888
GertCell <gertseli51@gmail.com>
2022-12-10 19:19:15 +0700

    rename. deleted

---
+++ Task 1/Machine epsilon.py
@@ -0,0 +1,23 @@
+# Epsilon
+
+e = 1
+i = 0
+
+while e + 1 != 1:
+    e = e/2
+    i = i + 1
+
+print('number of iteration and e', i, e)
+
+a = float(1)
+while a / 10 != a:
+    a = a * 10
+    i = i + 1
+
+print(a, i)
+
+print(1+2*e+e == 1+2*e)
+
+# print()
+
+
